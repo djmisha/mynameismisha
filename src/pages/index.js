f09f7db2bd92b43b448d1fce011c6655360efdf4
djmisha <misha.osinovskiy@gmail.comm>
2019-04-24 07:20:57 -0700

    spelling fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import AllPosts from "../components/allPosts"
 const IndexPage = () => (
   <Layout>
    		<SEO title="Misha Osinovskiy" keywords={[`web designer`,`web developer`, `DJ`, `Misha Osinovskiy`]} />
-   		 	<p>👋👋👋 Hello!  My name is Misha and I would like to welcome to my personal website.</p>
+   		 	<p>👋👋👋 Hello!  My name is Misha and I would like to welcome you to my personal website.</p>
    		 	<p>As a Web Developer at Rosemont Media I build websites that look great, load fast, get found on search and function seamlessly on any device.</p>
    		 	<p>As a San Diego DJ I perform at parties, corporate events and weddings playing dance music. <br /> Next event: Uncorked Wine Fest Del Mar - June 2019</p>
    		 	<p>You can explore here to learn a little more about me. Thank you for stopping by!</p>
