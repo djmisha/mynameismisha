d7e312a1dd53070aa5d6192126f181c673149768
djmisha <misha.osinovskiy@gmail.comm>
2019-04-24 07:52:45 -0700

    spelling

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
    		<SEO title="Misha Osinovskiy" keywords={[`web designer`,`web developer`, `DJ`, `Misha Osinovskiy`]} />
    		 	<p>👋👋👋 Hello!  My name is Misha and I would like to welcome you to my personal website.</p>
    		 	<p>As a Web Developer at Rosemont Media I build websites that look great, load fast, get found on search and function seamlessly on any device.</p>
-   		 	<p>As a San Diego DJ I perform at parties, corporate events and weddings playing dance music. <br /> Next event: Uncorked Wine Fest Del Mar - June 2019</p>
+   		 	<p>As a San Diego DJ I perform at parties, corporate events and weddings playing dance music. <i> Next event: Uncorked Wine Fest Del Mar - June 2019</i></p>
    		 	<p>You can explore here to learn a little more about me. Thank you for stopping by!</p>
 			<p><br/><i>Read articles from the blog...</i></p>
     	 <AllPosts />
